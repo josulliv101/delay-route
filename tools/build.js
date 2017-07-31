18faddb44b842942fe47d9e43be870dea8a78e81
Joe Sullivan <josulliv101@gmail.com>
2017-07-30 21:01:00 -0400

    added lib dir to output path

--- tools/build.js
+++ tools/build.js
@@ -11,32 +11,32 @@ const exec = (command, extraEnv) =>
 
 console.log('Building CommonJS modules ...')
 
-exec('babel modules -d . --ignore __tests__', {
+exec('babel modules -d ./lib --ignore __tests__', {
   BABEL_ENV: 'cjs'
 })
 
 console.log('\nBuilding ES modules ...')
 
-exec('babel modules -d es --ignore __tests__', {
+exec('babel modules -d ./lib/es --ignore __tests__', {
   BABEL_ENV: 'es'
 })
 
 console.log('\nBuilding delay-route.js ...')
 
-exec('rollup -c -f umd -o umd/delay-route.js', {
+exec('rollup -c -f umd -o lib/umd/delay-route.js', {
   BABEL_ENV: 'umd',
   NODE_ENV: 'development'
 })
 
 console.log('\nBuilding delay-route.min.js ...')
 
-exec('rollup -c -f umd -o umd/delay-route.min.js', {
+exec('rollup -c -f umd -o lib/umd/delay-route.min.js', {
   BABEL_ENV: 'umd',
   NODE_ENV: 'production'
 })
 
 const size = gzipSize.sync(
-  fs.readFileSync('umd/delay-route.min.js')
+  fs.readFileSync('lib/umd/delay-route.min.js')
 )
 
 console.log('\ngzipped, the UMD build is %s', prettyBytes(size))
